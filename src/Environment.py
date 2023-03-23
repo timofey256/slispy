2127e64af1d25b2803ad576db0d6e3c91f0e97f1
schetilt <shetilin.timofey@gmail.com>
2023-03-23 17:46:30 +0100

    Implementing VirtualMemory 1. Added allocation in memory

--- src/Environment.py
+++ src/Environment.py
@@ -6,6 +6,7 @@ import math
 import operator as op
 import Types
 import StandardLibrary as stl
+import sys
 
 class Environment:
     """
@@ -68,3 +69,113 @@ class Environment:
             'round':   round,
             'symbol?': lambda x: isinstance(x, Types.Symbol),
             })
+
+"""
+struct BlockHeader { }
+
+V_Memory:
+    max heap size;
+    header size;
+
+    # Memory allocation interface:
+    malloc()
+    _request_memory()
+    _find_free_block()
+    _split_block() if free block is at least twice larger than needed
+
+    # Memory cleaning interface:
+    gc(environment)
+    _markAll() in environment
+    _sweep() through all virtual memory and delete unmarked
+"""
+
+class Header:
+    def __init__(self, block_size=0):
+        self.size = block_size
+        self.prev = None
+        self.next = None
+        self.is_free = True
+        self.is_marked = False
+
+    def __repr__(self):
+        return f"Size: {self.size}\nIs Free: {self.is_free}"
+
+MAX_HEAP_SIZE = 256
+HEADER_SIZE = sys.getsizeof(Header)
+
+class VirtualMemory:
+    last_block = None
+
+    def __init__(self):
+        self.heap_size = 0
+        self.first_obj = None
+        self.objects_amount = 0
+        self.heap = [None for i in range(MAX_HEAP_SIZE)]
+        self.first_block = None
+
+    def malloc(self, size):
+        block = None
+
+        if size <= 0:
+            return None
+
+        size += HEADER_SIZE
+        
+        if self.first_block == None:
+            block = self.request_memory(size)
+            if block == None:
+                    return None
+            self.first_block = block
+        else:
+            block = self.find_free_block(self.first_block, size)
+            if block == None:
+                block = self.request_memory(size)
+                if block == None:
+                    return None
+            else:
+                block.is_free = False
+
+        self.heap_size += 1
+        self.objects_amount += 1
+        self.first_obj = block
+        self.heap[self.heap_size+1] = block
+
+        VirtualMemory.last_block = block
+        return block
+    
+    def request_memory(self, size):
+        block = Header(size)
+        block.next = None
+        block.is_free = False
+
+        if VirtualMemory.last_block:
+            VirtualMemory.last_block.next = block
+            block.prev = VirtualMemory.last_block
+        else:
+            self.first_block = block
+
+        VirtualMemory.last_block = block    
+
+        return block
+
+    def find_free_block(self, start_block, size):
+        current = start_block
+
+        while (current):
+            if current.is_free and current.size >= size:
+                if current.size >= 2*size:
+                    current = self.split_block(current, size)  
+                return current
+            
+            current = current.next
+
+        return None
+
+    def split_block(block, size):
+        new_block = Header(block.size - size)
+        new_block.next = block.next
+        new_block.is_free = True
+
+        block.size = size
+        block.next = new_block
+        return block
