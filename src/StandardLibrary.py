0bee92af742704a27d247d10362d8ff92d50d241
schetilt <shetilin.timofey@gmail.com>
2023-09-06 22:54:37 +0200

    Extended Standard library with logical AND and OR

--- src/StandardLibrary.py
+++ src/StandardLibrary.py
@@ -1,7 +1,21 @@
 def setcar(x, value):
-    x[0] = value
+    x.car = value
     return x
 
 def setcdr(x, value):
-    x[1:] = value
+    x.cdr = value
     return x
+
+def logical_or(*args):
+    res = False
+    for arg in args:
+        res = res or arg
+
+    return res
+
+def logical_and(*args):
+    res = True
+    for arg in args:
+        res = res and arg
+
+    return res
