1a65ecbbbb812cd3aa9bfbd2eb89decf122b707d
schetilt <shetilin.timofey@gmail.com>
2023-09-07 16:24:45 +0200

    Added conversion of Cons to Python list

--- src/Types.py
+++ src/Types.py
@@ -5,9 +5,15 @@ Types which are used in this Lisp-like language implementation.
 class ConsCell:
     def __init__(self, args):
         if len(args) > 2: raise Exception("Too many arguments for cons")
-        print(f'\tConsCell args : {args}')
         self.car = args[0]
         self.cdr = args[1]
+
+    def to_list(self):
+        l = [self.car]
+        if (isinstance(self.cdr, ConsCell)):
+            return l + self.cdr.to_list()
+        else: 
+            return l 
 
 Symbol = str
 Cons = ConsCell 
