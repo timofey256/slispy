51d34da548201c46bfa7fae08cc2d77426030997
schetilt <shetilin.timofey@gmail.com>
2023-09-07 16:22:43 +0200

    Added common Environment for test cases and fixed some testcases for bugs

--- src/Tests.py
+++ src/Tests.py
@@ -5,6 +5,8 @@ from Parser import Parser
 from Eval import eval, Procedure
 from Environment import Environment
 
+e = Environment()
+
 class TestSchemeTokenizer(unittest.TestCase):
     def test_tokenize_simple_expression(self):
         expression = "(+ 2 3)"
@@ -31,7 +33,6 @@ class TestSchemeTokenizer(unittest.TestCase):
         self.assertEqual(t.tokenize(expression), expected_tokens)
 
 class TestSchemeParser(unittest.TestCase):
-
     def test_parse_simple_expression(self):
         tokens = ['(', '+', '2', '3', ')']
         expected_ast = ['+', 2, 3]
@@ -65,40 +66,44 @@ class EvalTestCase(unittest.TestCase):
     def test_numbers(self):
         self.assertEqual(eval(self.get_AST("1")), 1)
         self.assertEqual(eval(self.get_AST("-1")), -1)
-        self.assertEqual(eval(self.get_AST("3.14")), 3.14)
+        self.assertEqual(eval(self.get_AST("3.14"),e), 3.14)
         
     def test_addition(self):
         self.assertEqual(eval(self.get_AST("(+ 1 2)")), 3)
-        self.assertEqual(eval(self.get_AST("(+ 1 (+ 2 3))")), 6)
+        self.assertEqual(eval(self.get_AST("(+ 1 (+ 2 3))"),e), 6)
         
     def test_subtraction(self):
         self.assertEqual(eval(self.get_AST("(- 1 2)")), -1)
-        self.assertEqual(eval(self.get_AST("(- 7 3)")), 4)
+        self.assertEqual(eval(self.get_AST("(- 7 3)"),e), 4)
         
     def test_multiplication(self):
         self.assertEqual(eval(self.get_AST("(* 2 3)")), 6)
-        self.assertEqual(eval(self.get_AST("(* 2 (* 3 4))")), 24)
+        self.assertEqual(eval(self.get_AST("(* 2 (* 3 4))"),e), 24)
         
     def test_division(self):
         self.assertEqual(eval(self.get_AST("(/ 8 2)")), 4)
         self.assertEqual(eval(self.get_AST("(/ 21 7)")), 3)
         
     def test_combined_operations(self):
-        self.assertEqual(eval(self.get_AST("(+ (* 2 3) (/ 6 2))")), 9)
+        self.assertEqual(eval(self.get_AST("(+ (* 2 3) (/ 6 2))"),e), 9)
 
     def test_lambda_functions(self):
-        self.assertEqual(eval(self.get_AST("((define circle-area (lambda (r) (* pi (* r r)))) (circle-area 3))")), 28.274333882308138)
-        self.assertEqual(eval(self.get_AST("((define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1)))))) (fact 10))")), 3628800)
-        self.assertEqual(eval(self.get_AST("""(                 
-        (define first car) 
-        (define rest cdr) 
-        (define count (lambda (item L) (if L (+ (equal? item (first L)) (count item (rest L))) 0)))
-        (count 0 (list 0 1 2 3 0 0))
-        )""")), 3)
-        self.assertEqual(list(eval(self.get_AST("""(                 
+        self.assertEqual(eval(self.get_AST("((define circle-area (lambda (r) (* pi (* r r)))) (circle-area 3))"),e), 28.274333882308138)
+        self.assertEqual(eval(self.get_AST("((define fact (lambda (n) (if (<= n 1) 1 (* n (fact (- n 1)))))) (fact 10))"),e), 3628800)
+        self.assertEqual(eval(self.get_AST("""(
+            (define first car)
+            (define rest cdr)
+            (define count (lambda (item L)
+                (if (and (not (number? L)) (not (null? L)))
+                    (+ (equal? item (first L)) (count item (rest L)))
+                    0)))
+            (count 0 (cons 0 (cons 1 (cons 2 (cons 3 (cons 0 (cons 0 1)))))))
+            )"""),e), 3)
+        self.assertEqual((eval(self.get_AST("""(                 
         (define range (lambda (a b) (if (= a b) (quote ()) (cons a (range (+ a 1) b)))))
         (range 0 10)
-        )"""))), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+        )"""),e)).to_list(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
 
 class TestMemoryManagement(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
